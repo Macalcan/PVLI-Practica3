cb28735442a64f7916a8a2bd26be7f51c056dc3a
BMZE <blamacaz@ucm.es>
2017-01-08 14:41:30 +0100

    debido a problemas con phaser antes no se ha podido hacer un commit, este commit contiene un juego basico con sprites temporales y las mecanicas esenciales

---
+++ .tmp/js/bundle.js
@@ -0,0 +1,27 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+window.onload = function () {
+
+	var game = new Phaser.Game(800, 600, Phaser.CANVAS, '');
+
+	var GameState = {
+
+		preload: function() {
+
+		},
+		create: function() {
+
+		},
+		update: function() {
+
+		}, 
+	};
+
+	game.state.add('Boot', Game.Boot);
+	game.state.add('Preloader', Game.Preloader);
+	game.state.add('MainMenu', Game.MainMenu);
+	game.state.add('Level1', Game.Level1);
+
+	game.state.start('Boot');
+
+}
+},{}]},{},[1]);
---
+++ src/js/Level1.js
@@ -0,0 +1,252 @@
+EnemyBird = function (index, game, x, y) {
+	
+	this.bird = game.add.sprite(x, y, 'bird');
+	this.bird.anchor.setTo(0.5, 0.5);
+	this.bird.name = index.toString();
+	game.physics.enable(this.bird, Phaser.Physics.ARCADE);
+	this.bird.body.immovable = true;
+	this.bird.body.collideWorldBounds = true;
+	this.bird.body.allowGravity = false;
+
+	this.birdTween = game.add.tween(this.bird).to({
+		y: this.bird.y + 100
+	}, 2000, 'Linear', true, 0, 100, true);
+
+};
+
+
+var enemy1;
+
+
+Game.Level1 = function(game) {};
+
+var map;
+var layer;
+
+var player;
+var control = {};
+var playerSpeed = 150;
+var jumpTimer = 0;
+
+var button;
+
+var drag;
+
+var shootTime = 0;
+var nuts;
+
+var respawn;
+
+var playerXP = 0;
+var gameXPsteps = 15;
+
+var playerLevel = 0;
+
+
+var padXBOX;
+var buttonA;
+var buttonX;
+var jumping;
+var shooting;
+
+Game.Level1.prototype = {
+
+	create: function (game) {
+		
+		this.stage.backgroundColor = '#3A5963';
+
+		this.physics.arcade.gravity.y = 1400;
+
+		respawn = game.add.group();
+
+		map = this.add.tilemap('map');
+
+		map.addTilesetImage('tileset', 'tileset');
+
+		layer = map.createLayer('Capa de Patrones 1');
+
+		layer.resizeWorld();
+
+		map.setCollisionBetween(0,3);
+
+		map.setTileIndexCallback(7, this.getCoin, this);
+
+		map.setTileIndexCallback(6, this.spawn, this);
+
+		map.createFromObjects('Capa de Objetos 1', 8, '', 0, true, false, respawn);
+
+
+
+		player = this.add.sprite(0, 0, 'player');
+		player.anchor.setTo(0.5, 0.5);
+
+		this.spawn();
+
+		player.animations.add('idle', [0,1], 2, true);
+		player.animations.add('jump', [2], 1, true);
+		player.animations.add('run', [3, 4, 5, 6, 7, 8], 7, true);
+
+		this.physics.arcade.enable(player);
+		this.camera.follow(player);
+		player.body.colliderWorldBounds = true;
+
+		controls = {
+			right: this.input.keyboard.addKey(Phaser.Keyboard.D),
+			left: this.input.keyboard.addKey(Phaser.Keyboard.A),
+			up: this.input.keyboard.addKey(Phaser.Keyboard.W),
+			shoot: this.input.keyboard.addKey(Phaser.Keyboard.UP),
+		};
+
+		button = this.add.button(this.world.centerX - 95, this.world.centerY + 200, 'buttons', function(){
+			console.log('pressed');
+		}, this, 2, 1, 0);
+
+		button.fixedToCamera = true;
+
+		drag = this.add.sprite(player.x, player.y, 'drag');
+		drag.anchor.setTo(0.5, 0.5);
+		drag.inputEnabled = true;
+		drag.input.enableDrag(true);
+		this.physics.arcade.enable(drag);
+		drag.body.colliderWorldBounds = true;
+
+		enemy1 = new EnemyBird(0, game, player.x + 400, player.y - 200);
+
+		nuts = game.add.group();
+		nuts.enableBody = true;
+		nuts.physicsBodyType = Phaser.Physics.ARCADE;
+		nuts.createMultiple(5, 'nut');
+		nuts.setAll('anchor.x', 0.5);
+		nuts.setAll('anchor.y', 0.5);
+		nuts.setAll('scale.x', 0.5);
+		nuts.setAll('scale.y', 0.5);
+		nuts.setAll('outOfBoundsKill', true);
+		nuts.setAll('checkWorldBounds', true);
+
+
+		game.input.gamepad.start();
+		padXBOX = game.input.gamepad.pad1;
+		padXBOX.addCallbacks(this, {onConnect: this.addButons});
+
+		
+	},
+
+	update: function() {
+
+		this.physics.arcade.collide(player, layer);
+		this.physics.arcade.collide(player, enemy1.bird, this.resetPlayer);
+
+		player.body.velocity.x = 0;
+		
+		playerLevel = Math.log(playerXP, gameXPsteps);
+		console.log('Level: ' + Math.floor(playerLevel));
+
+
+		if (controls.right.isDown || (padXBOX.isDown(Phaser.Gamepad.XBOX360_DPAD_RIGHT) || 
+			padXBOX.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > 0.1)) {
+			if (player.body.onFloor() ||player.body.touching.down)
+				player.animations.play('run');
+			player.scale.setTo(1, 1);
+			player.body.velocity.x += playerSpeed;
+		}
+
+		if (controls.left.isDown || (padXBOX.isDown(Phaser.Gamepad.XBOX360_DPAD_LEFT) || 
+			padXBOX.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -0.1)) {
+			if (player.body.onFloor() ||player.body.touching.down)
+				player.animations.play('run');
+			player.scale.setTo(-1, 1);
+			player.body.velocity.x -= playerSpeed;
+		}
+
+		if ((controls.up.isDown || jumping) && (player.body.onFloor() ||
+			player.body.touching.down) && this.time.now > jumpTimer) {
+			player.body.velocity.y = -600;
+			jumpTimer = this.time.now + 750;
+			player.animations.play('jump');
+		}
+
+		if (player.body.velocity.x == 0 && player.body.velocity.y == 0) {
+			player.animations.play ('idle');
+		}
+
+		if(controls.shoot.isDown || shooting) {
+			this.shootNut();
+
+		} 
+
+		if (checkOverlap(nuts, enemy1.bird)) {
+			enemy1.bird.kill();
+		}
+
+	},
+
+	resetPlayer: function() {
+
+		player.reset(100, 560);
+	},
+
+	spawn: function() {
+
+		respawn.forEach(function(spawnPoint){
+
+			player.reset(spawnPoint.x, spawnPoint.y);
+
+		}, this);
+	},
+
+	getCoin: function() {
+
+		map.putTile(-1, layer.getTileX(player.x), layer.getTileY(player.y));
+
+		playerXP += 15;
+	},
+
+	shootNut: function() {
+		if(this.time.now > shootTime) {
+			nut = nuts.getFirstExists(false);
+			if(nut) {
+				nut.reset(player.x, player.y);
+
+				nut.body.velocity.y = -600;
+
+				shootTime = this.time.now + 900;
+
+				playerXP += 15;
+			}
+		}
+	},
+
+
+
+	addButons: function () {
+		buttonA = padXBOX.getButton(Phaser.Gamepad.XBOX360_A);
+		buttonX = padXBOX.getButton(Phaser.Gamepad.XBOX360_X);
+
+		buttonA.onDown.add(function(){
+			jumping = true;
+		}, this);
+
+		buttonA.onUp.add(function(){
+			jumping = false;
+		}, this);
+
+		buttonX.onDown.add(function(){
+			shooting = true;
+		}, this);
+
+		buttonX.onUp.add(function(){
+			shooting = false;
+		}, this);
+	},
+
+
+};
+
+
+function checkOverlap (spriteA, spriteB) {
+	
+	var boundsA = spriteA.getBounds();
+	var boundsB = spriteB.getBounds();
+
+	return Phaser.Rectangle.intersects(boundsA, boundsB);
+}
